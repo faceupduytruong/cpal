8733326e9e74ff2b24bf5bf2d5f49ebc5770b3fa
faceupduytruong <khobauonepiece@gmail.com>
2025-08-12 13:28:14 +0700

    Update resSources.js

--- docs/resSources.js
+++ docs/resSources.js
@@ -1,3 +1,4 @@
+// File: docs/resSources.js
 export const resSources = [
   "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-1e0d0a4f1d0f9471ace0566034009306-1920x1080_w8ogmb.mp4",
   "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-f0ea3cc1ab88f56b1edc1a26863273ed-1920x1080_lox6dh.mp4",
