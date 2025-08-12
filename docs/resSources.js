2eca7325f29681dbae6865326ce297e0ce59d154
faceupduytruong <khobauonepiece@gmail.com>
2025-08-12 14:05:33 +0700

    Update resSources.js

--- docs/resSources.js
+++ docs/resSources.js
@@ -1,6 +1,6 @@
 // File: docs/resSources.js
 export const resSources = [
-  "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-fbc1b84596d7ae897d0c6acb07313e58-1920x1080_svnhkf.mp4",
+"https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-fbc1b84596d7ae897d0c6acb07313e58-1920x1080_svnhkf.mp4",
 "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-f0ea3cc1ab88f56b1edc1a26863273ed-1920x1080_lox6dh.mp4",
 "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-e19daab02953e4260812f5963f18df6c-1920x1080_gpi66p.mp4",
 "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754981930/37-c766d78c3d9150ac0b84517335b7972e-1920x1080_junbjd.mp4",
@@ -11,4 +11,4 @@ export const resSources = [
 "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754981930/37-25b677b7133e20d28a3bdb8aaaeb897e-1920x1080_t4aewx.mp4",
 "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754981930/37-1e0d0a4f1d0f9471ace0566034009306-1920x1080_w8ogmb.mp4",
 "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754981930/37-05c8c61b4c758d4c29cd166ac7d7d9a7-1920x1080_j9xbh2.mp4",
-  ];
+];
