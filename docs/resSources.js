73f3b17dd8c0adb3217781bd3e2d85efa61a50e6
faceupduytruong <khobauonepiece@gmail.com>
2025-08-12 13:11:51 +0700

    Create resSources.js

---
+++ docs/resSources.js
@@ -0,0 +1,5 @@
+export const videoSources = [
+  "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-1e0d0a4f1d0f9471ace0566034009306-1920x1080_w8ogmb.mp4",
+  "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-f0ea3cc1ab88f56b1edc1a26863273ed-1920x1080_lox6dh.mp4",
+  "https://res.cloudinary.com/dq7c3wdhf/video/upload/f_auto,q_auto/v1754972596/37-98c5ba80f61d3af1fddde77e25cc5f7b-1920x1080_uov4rm.mp4"
+];
