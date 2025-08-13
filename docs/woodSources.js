307625d7010aebc6c7cbf2babff667c33c8f400b
faceupduytruong <khobauonepiece@gmail.com>
2025-08-13 20:44:21 +0700

    Update woodSources.js

--- docs/woodSources.js
+++ docs/woodSources.js
@@ -1,10 +1,21 @@
 export const woodSources = [
+"https://assets.mixkit.co/videos/52298/52298-720.mp4",
+  
 "https://assets.mixkit.co/videos/12277/12277-720.mp4",
 "https://assets.mixkit.co/videos/263/263-720.mp4",
 "https://assets.mixkit.co/videos/4591/4591-720.mp4",
 "https://assets.mixkit.co/videos/817/817-720.mp4",
 "https://assets.mixkit.co/videos/14189/14189-720.mp4",
 "https://assets.mixkit.co/videos/45831/45831-720.mp4",
-"https://assets.mixkit.co/videos/50729/50729-720.mp4",
+"https://assets.mixkit.co/videos/17384/17384-720.mp4",
+"https://assets.mixkit.co/videos/15897/15897-720.mp4",
+"https://assets.mixkit.co/videos/18797/18797-720.mp4",
+"https://assets.mixkit.co/videos/8826/8826-720.mp4",
+"https://assets.mixkit.co/videos/45114/45114-720.mp4",
+"https://assets.mixkit.co/videos/14542/14542-720.mp4",
+"https://assets.mixkit.co/videos/18782/18782-720.mp4",
+"https://assets.mixkit.co/videos/14389/14389-720.mp4",
+"https://assets.mixkit.co/videos/33991/33991-720.mp4",
+"https://assets.mixkit.co/videos/27180/27180-720.mp4",
 "http://p3-scmimg.bytescm.com/obj/tos-cn-i-rn3s1tazwm/ies/musician_storm/static/media/end.d4a6b496.mp4"
   ];
