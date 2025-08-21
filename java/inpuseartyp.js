d3f15346ccd7b3c83d431ebf3c91f9a2ec877404
faceupduytruong <khobauonepiece@gmail.com>
2025-08-21 18:50:12 +0700

    Update inpuseartyp.js

--- java/inpuseartyp.js
+++ java/inpuseartyp.js
@@ -4,15 +4,26 @@
     const keyword = suggestions[randomIndex];
     document.getElementById("searchInput").value = keyword;
     }
+
+ // 👉 Ngăn không cho nhập dấu cách
     function blockSpace(e) {
     if (e.key === ' ') {
       e.preventDefault(); // Ngăn không cho nhập dấu cách
     }
   }
 
-
   <!-- 👉 Xóa ô nhập vào ô tìm kiếm -->
   // Hàm xóa nội dung ô nhập
   document.getElementById("DelBtn").onclick = function() {
     document.getElementById("searchInput").value = "";
-  };
+  }
+
+// 👉 Dán nội dung từ clipboard
+document.getElementById("PasteBtn").onclick = async function() {
+  try {
+    const text = await navigator.clipboard.readText();
+    document.getElementById("searchInput").value = text;
+  } catch (err) {
+    alert("Không thể truy cập clipboard. Hãy đảm bảo bạn đã cấp quyền.");
+  }
+};
