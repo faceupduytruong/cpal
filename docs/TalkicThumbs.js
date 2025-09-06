33702bdade027c491d0f6418f7b681eafa93fea5
faceupduytruong <khobauonepiece@gmail.com>
2025-09-06 16:35:26 +0700

    Add files via upload

---
+++ docs/TalkicThumbs.js
@@ -0,0 +1,15 @@
+const customThumbnails = {
+  Art: 'https://maludesign.vn/wp-content/uploads/2022/08/illustration-sandro-rybak-feature.jpg',
+  Contrast: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbtizEhJchxqVlPA8tXuneisjq-Z1RLcj48A&s',
+  Correct: 'https://atlassianblog.wpengine.com/wp-content/uploads/2020/02/1120x545-4-1560x760.jpg',
+  Employment: 'https://static.vecteezy.com/system/resources/thumbnails/000/174/133/small_2x/Job_Search_in_Newspaper.jpg',
+  Food: 'https://hips.hearstapps.com/hmg-prod/images/fresh-healthy-organic-food-for-balanced-diet-on-royalty-free-image-1738258703.pjpeg?crop=1xw:0.84335xh;center,top&resize=1200:*',
+  Grammar: 'https://ryteup.com/img/english-grammar.jpeg',
+  Health: 'https://files.websitebuilder.prositehosting.co.uk/f1/8c/f18ce48a-79d0-4cab-8830-1f06852deb70.jpg',
+  Kids: 'https://nationaltoday.com/wp-content/uploads/2022/07/76u-min-1200x834.jpg',
+  Preposition: 'https://www.trinka.ai/blog/wp-content/uploads/2020/09/Prepositions-1-1200x688.png',
+  Sentence: 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2021/04/types-of-sentences.jpeg',
+  Time: 'https://cdn.mos.cms.futurecdn.net/9meMCxtJU3NWkh5YcPs7qG-1200-80.jpg',
+  Word: 'https://www.customguide.com/images/cover-images/word.webp',
+  // thêm các mục khác nếu cần
+};
