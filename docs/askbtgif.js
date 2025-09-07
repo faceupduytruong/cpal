f4b72874fba428cd7c44c54ea241cdf18c9cd895
faceupduytruong <khobauonepiece@gmail.com>
2025-09-07 14:33:45 +0700

    Add files via upload

---
+++ docs/askbtgif.js
@@ -0,0 +1,39 @@
+// Danh sách ảnh GIF
+window.gifList = [
+    "https://cdn.dribbble.com/userupload/42316282/file/original-b27d6c2fb95015bd3468560159af00a6.gif",
+    "https://cdn.dribbble.com/userupload/31926138/file/original-e1c94c0fc82199aab49fa407bb74257b.gif",
+    "https://i.pinimg.com/originals/b2/a3/e8/b2a3e872dfb354f73ee039a879d853f9.gif",
+    "https://cdn.dribbble.com/userupload/21897811/file/original-28ad4207577eef7e70d245b9a2cb9b56.gif",
+    "https://cdn.dribbble.com/userupload/22081279/file/original-55732f1cbe5258b25566b001c5267a33.gif",
+    "https://i.pinimg.com/originals/af/22/e2/af22e248b58627c543742432c4bde2a8.gif",
+    "https://mir-s3-cdn-cf.behance.net/project_modules/hd/f4020f114494633.603cbcc781296.gif",
+    "https://i.pinimg.com/originals/eb/1c/d6/eb1cd6dc77d7c9ce7a10b8503f065bea.gif",
+    "https://i.pinimg.com/originals/9c/da/64/9cda6405c91f0a6060592638e58932c4.gif",
+    "https://i.pinimg.com/originals/c1/14/12/c114123eb48fc0712b2482264414d9d7.gif",
+    "https://i.pinimg.com/originals/af/7b/6e/af7b6ee82ae6de2df640d6d40c8fe8a4.gif",
+    "https://storage.googleapis.com/buro-malaysia-storage/www.buro247.my/2024/09/b3e6c251-01-standart_appleintelligence.gif",
+    "https://havo-23.informaticaweb.nl/images/siri.gif",
+    "https://cdn.dribbble.com/userupload/23662114/file/original-e1b947cb36707ae1cec1023eddafec04.gif", 
+    "https://cdn.dribbble.com/userupload/24309342/file/original-3cbf9a8e9a1e8695365bd0adc4f48a9c.gif",
+    "https://i.pinimg.com/originals/ff/04/31/ff0431d11ff6b73e937280252f58f371.gif",
+    "https://i.pinimg.com/originals/70/eb/0b/70eb0b53eb57c91db403928c5d02a19a.gif",
+    "https://i.pinimg.com/originals/84/c6/3f/84c63f9bb767b8a8705c0360baaa2f74.gif",
+    "https://cdn.dribbble.com/userupload/42334196/file/original-6371f93570580226629c7676244eafef.gif",   
+    "https://cdn.dribbble.com/userupload/42409823/file/original-568544560f6ca1076a16e3428302e329.gif",
+    "https://mir-s3-cdn-cf.behance.net/project_modules/hd/b612a152965125.5d01be417c2ad.gif",
+    "https://cdn.dribbble.com/userupload/23714478/file/original-245a8f90b4b789450b27be0876dea4d8.gif",
+    "https://i.pinimg.com/originals/ae/d1/1d/aed11d6975231b91c8e992c02b8376da.gif",
+    "https://i.pinimg.com/originals/19/e4/21/19e421ce92a12f4bfdc1171abb28486c.gif",
+    "https://i.pinimg.com/originals/dc/7b/49/dc7b49b3f913908b7556f8a34c338bd3.gif",
+    "https://i.pinimg.com/originals/7a/9b/80/7a9b8077fd3b55d8ab72bdb6864f9448.gif",
+    "https://i.pinimg.com/originals/eb/bd/f7/ebbdf7ce4f7f502d1f28b96b5cbd7a1f.gif",
+    "https://i.pinimg.com/originals/06/f6/2e/06f62ec38e29a0596709a21a5dc22e8a.gif",
+    "https://i.pinimg.com/originals/ab/a3/cc/aba3cc4236be4c67f46a0e7026bce60c.gif",
+    "https://mir-s3-cdn-cf.behance.net/project_modules/hd/faaa9a62816725.5a9d098f58be0.gif",
+    "https://about.fb.com/wp-content/uploads/2024/09/Connect-24_AI-Updates_Header.gif",
+    "https://cdnb.artstation.com/p/assets/images/images/018/467/105/original/ivan-boyko-energy-orb-art.gif?1559502600",
+    "https://images.squarespace-cdn.com/content/v1/54bdcba5e4b08f92b173441f/f34f7c2a-4226-4145-aea9-a6890d9c7990/Apple-Event-iphone16.gif"
+  ];
+
+
+
