45217086b93e373949276508c00e41cff63c2b13
faceupduytruong <khobauonepiece@gmail.com>
2025-09-12 09:53:40 +0700

    Update 2bttnplahlsturl.js

--- java/2bttnplahlsturl.js
+++ java/2bttnplahlsturl.js
@@ -1,17 +1,32 @@
-  const video = document.getElementById('player');
-  const playBtn = document.getElementById('playBtn');
-  const stopBtn = document.getElementById('stopBtn');
-  const hls = new Hls();
-  const streamUrl = 'https://vnso-pt-8-tf-multi-playlist-zmp3.zmdcdn.me/nDOW7B2Y4NI/zhls/playback-realtime/audio/97b35e1f625a8b04d24b/audio.m3u8';
+// VIDEO PLAYER SETUP
+const video = document.getElementById('player');
+const playBtn = document.getElementById('playBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoHls = new Hls();
+const videoStreamUrl = 'https://vnso-pt-8-tf-multi-playlist-zmp3.zmdcdn.me/nDOW7B2Y4NI/zhls/playback-realtime/audio/97b35e1f625a8b04d24b/audio.m3u8';
 
-  hls.loadSource(streamUrl);
-  hls.attachMedia(video);
-  video.play();              // Phát tự động
-
+if (Hls.isSupported()) {
+  videoHls.loadSource(videoStreamUrl);
+  videoHls.attachMedia(video);
+  video.play(); // Tự động phát
 
   playBtn.onclick = () => { video.play(); };
   stopBtn.onclick = () => {
     video.pause();
     video.currentTime = 0;
-
   };
+}
+
+// AUDIO PLAYER SETUP
+const audio = document.getElementById('audio');
+const songInfo = document.getElementById('songInfo');
+const audioHls = new Hls();
+const audioStreamUrl = 'https://vnso-pt-8-tf-multi-playlist-zmp3.zmdcdn.me/nDOW7B2Y4NI/zhls/playback-realtime/audio/97b35e1f625a8b04d24b/audio.m3u8';
+
+if (Hls.isSupported()) {
+  audioHls.loadSource(audioStreamUrl);
+  audioHls.attachMedia(audio);
+  audioHls.on(Hls.Events.MANIFEST_PARSED, () => {
+    audio.play();
+  });
+}
