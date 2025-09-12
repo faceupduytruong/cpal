1a4ac5da60016f5625be767b77d9d786a319be11
faceupduytruong <khobauonepiece@gmail.com>
2025-09-12 08:18:13 +0700

    Add files via upload

---
+++ java/2bttnplaysturl.js
@@ -0,0 +1,16 @@
+  const video = document.getElementById('player');
+  const playBtn = document.getElementById('playBtn');
+  const stopBtn = document.getElementById('stopBtn');
+  const hls = new Hls();
+  const streamUrl = 'https://vnso-pt-8-tf-multi-playlist-zmp3.zmdcdn.me/nDOW7B2Y4NI/zhls/playback-realtime/audio/97b35e1f625a8b04d24b/audio.m3u8';
+
+  hls.loadSource(streamUrl);
+  hls.attachMedia(video);
+  video.play();              // Phát tự động
+
+
+  playBtn.onclick = () => { video.play(); };
+  stopBtn.onclick = () => {
+    video.pause();
+    video.currentTime = 0;
+  };
