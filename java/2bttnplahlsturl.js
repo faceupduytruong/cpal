5bee38321a933020f7b4e81c9d98ebb129213fbb
faceupduytruong <khobauonepiece@gmail.com>
2025-09-12 10:23:58 +0700

    Update 2bttnplahlsturl.js

--- java/2bttnplahlsturl.js
+++ java/2bttnplahlsturl.js
@@ -20,23 +20,4 @@ if (Hls.isSupported()) {
   };
 }
 
-// AUDIO PLAYER SETUP
-    const audio = document.getElementById('audio');
-    const audioHls = new Hls();
-    const streamURL = "https://vnso-pt-8-tf-multi-playlist-zmp3.zmdcdn.me/nDOW7B2Y4NI/zhls/playback-realtime/audio/97b35e1f625a8b04d24b/audio.m3u8";
-
-    if (Hls.isSupported()) {
-      audioHls.loadSource(streamURL);
-      audioHls.attachMedia(audio);
-      audioHls.on(Hls.Events.MANIFEST_PARSED, () => {
-        audio.play();
-      });
-    } else if (audio.canPlayType('application/vnd.apple.mpegurl')) {
-      audio.src = streamURL;
-      audio.addEventListener('loadedmetadata', () => {
-        audio.play();
-      });
-    }
-
 });
-
