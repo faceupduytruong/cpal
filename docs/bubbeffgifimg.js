e01976724c48b48f4217d6c07cd2399127eac86e
faceupduytruong <khobauonepiece@gmail.com>
2025-09-14 17:13:07 +0700

    Create bubbeffgifimg.js

---
+++ docs/bubbeffgifimg.js
@@ -0,0 +1,24 @@
+  // Danh sách ảnh GIF bong bóng
+ export const bubbleEffects = [
+   'https://i.pinimg.com/originals/ed/2b/38/ed2b38699271472bdaf6ebd08531ef42.gif',
+   'https://i.pinimg.com/originals/ca/97/bd/ca97bde328433c2497b154afdee5f8d7.gif',
+   'https://img1.picmix.com/output/stamp/normal/3/1/2/0/2500213_84015.gif',    
+   'https://i.pinimg.com/originals/0a/ed/71/0aed71900f1c301d7506a485bce60a37.gif',
+   'https://media.tenor.com/5GVsInsrd_YAAAAj/love-you-emojis.gif',
+   'https://i.pinimg.com/originals/13/f8/b6/13f8b6e6795cb74b42db4b1fc3e5b21b.gif',
+   'https://cdn.pixabay.com/animation/2025/01/06/10/20/10-20-24-622_512.gif',
+   'https://img1.picmix.com/output/stamp/normal/7/6/2/8/2568267_0d1df.gif',
+   'https://i.pinimg.com/originals/ad/17/ce/ad17ce3333423aec8b1bf313f11681f5.gif',
+   'https://img1.picmix.com/output/stamp/normal/3/7/5/0/1750573_71510.gif',
+   'https://clipart-library.com/img/1469754.gif',
+   'https://i.pinimg.com/originals/0f/22/16/0f22164d3270216b134692ad165835ac.gif',
+   'https://i.pinimg.com/originals/0b/93/bb/0b93bbbd523fe0bb676576f8ce87828c.gif',
+   'https://media.tenor.com/9lJUggaNRZkAAAAj/purple-heart.gif',
+   'https://img1.picmix.com/output/stamp/normal/4/6/5/4/2444564_f86a6.gif',
+   'https://cdn.pixabay.com/animation/2024/03/13/01/40/01-40-40-44_512.gif',
+   'https://media.tenor.com/DEbTjK2sh4UAAAAj/heart.gif',
+   'https://i.pinimg.com/originals/f3/7e/bb/f37ebbea1f4318dec775a4d705bd7cca.gif',
+   'https://www.stmarys-sellyoak.co.uk/wp-content/uploads/earth-gif.gif',
+   'https://cdn.pixabay.com/animation/2023/06/21/15/53/15-53-17-555_512.gif',
+   'https://img1.picmix.com/output/stamp/normal/0/1/5/4/534510_e11ac.gif'
+  ];
