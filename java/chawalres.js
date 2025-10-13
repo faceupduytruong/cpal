d83a2eda982c2287de5143b0a1c9aef45f76a718
faceupduytruong <khobauonepiece@gmail.com>
2025-10-13 23:40:36 +0700

    Update chawalres.js

--- java/chawalres.js
+++ java/chawalres.js
@@ -1,4 +1,4 @@
-import { resSources } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/docs/resSources.js';
+import { resSources } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal@2c95fef/docs/resSources.js';
 
 // 👉 Chèn CSS vào DOM
 const style = document.createElement('style');
@@ -60,5 +60,6 @@ document.getElementById("toggleEffectBtn")?.addEventListener("click", () => {
   loadBackgroundVideo(currentIndex);
 
 });
+
 
 
