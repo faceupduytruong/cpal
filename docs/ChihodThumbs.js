0f7dc0e986434d20ecede970e479e5850ada63c9
faceupduytruong <khobauonepiece@gmail.com>
2026-01-11 17:36:58 +0700

    Update ChihodThumbs.js

--- docs/ChihodThumbs.js
+++ docs/ChihodThumbs.js
@@ -13,6 +13,10 @@ const folders = {
 };
 
 const folderLinks = {
+  'Sách Kể Chuyện': {
+    url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Story/index.htm',
+    thumbnail: 'https://www.dropbox.com/scl/fi/mzn88e97lpg5o4wgqw3cx/S-ch-K-Chuy-n.webp?rlkey=71udzkkrr0jw2plf77s72ebxg&st=zbnf1h9g&raw=1',
+  },
   'Google': {
     url: 'https://translate.google.com.vn/?lfhs=2&sl=en&tl=vi&op=translate',
     thumbnail: 'https://www.dropbox.com/scl/fi/6686b8zwwloagxpcjanql/Google-Translate.jpg?rlkey=62s6qak72385hqbiod8414f1b&st=suqz7c3v&raw=1'
