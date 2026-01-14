78c108069c9a4be92de763fb4c2570f195022ea5
faceupduytruong <khobauonepiece@gmail.com>
2026-01-14 08:25:43 +0700

    Update corobimagBox.js

--- docs/corobimagBox.js
+++ docs/corobimagBox.js
@@ -116,10 +116,31 @@ container.innerHTML = `
       </div>
 
       <div class="image-box">
+       <div onclick="openRightHalfPopup('https://www.facebook.com/macforce2601/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/f8r7u8em4scj448ozhwk4/Macforce2601.jpg?rlkey=4zlhyceeh2fmjotultc3eaf8b&st=s4yttig4&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Macforce2601</div>
+      </div>
+
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://hsvn.gitbook.io/guides/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/pvzvet37rlnxgpg2m820j/Hsvn.png?rlkey=72lcdj729y9pz2tve4vte6f84&st=695m3v6f&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Hsvn</div>
+      </div>
+
+      <div class="image-box">
        <div onclick="openRightHalfPopup('https://dortania.github.io/OpenCore-Install-Guide/prerequisites.html#prerequisites' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
        <img src="https://www.dropbox.com/scl/fi/vfqjcgmdcegd0mxqv2ob2/Dortania.png?rlkey=7xv922bzql8dd02kbhwwwsd71&st=17sv6b35&raw=1" alt="Ảnh 1">
 </div>
         <div class="caption">Dortania</div>
+      </div>
+
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://rampagedev.wordpress.com/kext-editing/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/qi0g2qng51l90sjq6bfmx/Rampagedev.jpg?rlkey=rhya0h4xenka6w6n9ui4aqte9&st=yspsbrwm&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Rampagedev</div>
       </div>
 
       <div class="image-box">
@@ -143,6 +164,41 @@ container.innerHTML = `
         <div class="caption">Insanelymac</div>
       </div>
 
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('http://www.macbreaker.com/2012/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/e1k41mh43l5hhtzwjrfmg/Macbreaker.jpg?rlkey=wdxf31wl01fpd4a7rbl5q1pz2&st=54o94jgs&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Macbreaker</div>
+      </div>
+
+     <div class="image-box">
+       <div onclick="openRightHalfPopup('https://techmaster.vn/posts?keyword=&category=88&author=' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/ymzmy3swhszvg79w1g9u6/Techmaster-VN-Mac-OSX.png?rlkey=blekdcukudwprtqfqyi8ieo4g&st=rxqwe42j&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Techmaster VN (Mac OSX)</div>
+      </div>
+
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('http://s.sudre.free.fr/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/3krsoxwhr88kxu2k1t9qp/Sudre.png?rlkey=cygh1vy5xpc22q6xhhsdplbzj&st=k1a2d63l&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Sudre</div>
+      </div>
+
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://brettterpstra.com/topic/macos/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/c93zzc2sziyxjrj08o9sv/Brettterpstra.png?rlkey=ws0aq1pz2fxb762tx60qmjr59&st=shb8fxxa&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Brettterpstra</div>
+      </div>
+
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://www.macbed.com/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://www.dropbox.com/scl/fi/d8gnak6jnmx65estx2t6i/Macbed.png?rlkey=yoqciaut41axy3z0ejdskpa8p&st=u4rmdo7w&raw=1" alt="Ảnh 1">
+</div>
+        <div class="caption">Macbed</div>
+      </div>
+      
       <div class="image-box">
        <div onclick="openRightHalfPopup('https://sharefreeall.com/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
        <img src="https://www.dropbox.com/scl/fi/h0tjxvjhft22i4ofhdlzw/Sharefreeall.webp?rlkey=rikjcyipu9io4dt3ugvp6wu7y&st=lh4zxarj&raw=1" alt="Ảnh 1">
@@ -205,27 +261,6 @@ container.innerHTML = `
       </div>
  
       <div class="image-box">
-       <div onclick="openRightHalfPopup('https://www.facebook.com/macforce2601/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/f8r7u8em4scj448ozhwk4/Macforce2601.jpg?rlkey=4zlhyceeh2fmjotultc3eaf8b&st=s4yttig4&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Macforce2601</div>
-      </div>
-
-      <div class="image-box">
-       <div onclick="openRightHalfPopup('https://hsvn.gitbook.io/guides/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/pvzvet37rlnxgpg2m820j/Hsvn.png?rlkey=72lcdj729y9pz2tve4vte6f84&st=695m3v6f&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Hsvn</div>
-      </div>
-
-      <div class="image-box">
-       <div onclick="openRightHalfPopup('https://rampagedev.wordpress.com/kext-editing/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/qi0g2qng51l90sjq6bfmx/Rampagedev.jpg?rlkey=rhya0h4xenka6w6n9ui4aqte9&st=yspsbrwm&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Rampagedev</div>
-      </div>
-
-      <div class="image-box">
        <div onclick="openRightHalfPopup('https://www.macworld.com/software' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
        <img src="https://www.dropbox.com/scl/fi/59cemdbhxpfdqbuiazf9s/Macworld.png?rlkey=spt485vs2g2jafygopqtsjbqg&st=ylev3u0w&raw=1" alt="Ảnh 1">
 </div>
@@ -240,20 +275,6 @@ container.innerHTML = `
       </div>
 
       <div class="image-box">
-       <div onclick="openRightHalfPopup('http://www.macbreaker.com/2012/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/e1k41mh43l5hhtzwjrfmg/Macbreaker.jpg?rlkey=wdxf31wl01fpd4a7rbl5q1pz2&st=54o94jgs&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Macbreaker</div>
-      </div>
-
-      <div class="image-box">
-       <div onclick="openRightHalfPopup('http://s.sudre.free.fr/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/3krsoxwhr88kxu2k1t9qp/Sudre.png?rlkey=cygh1vy5xpc22q6xhhsdplbzj&st=k1a2d63l&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Sudre</div>
-      </div>
-
-      <div class="image-box">
        <div onclick="openRightHalfPopup('https://brew.sh/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
        <img src="https://www.dropbox.com/scl/fi/srymwblefu3jcm1z2mxrq/Homebrew.png?rlkey=gjz8eelaar0p1pbj1fnqzn7zb&st=t2psfdaf&raw=1" alt="Ảnh 1">
 </div>
@@ -265,27 +286,6 @@ container.innerHTML = `
        <img src="https://www.dropbox.com/scl/fi/hdigselpfdciaqgfep76j/MacintoshVietNam.png?rlkey=m5fth2dh8cxj7x0v9tq0wv42h&st=u33yoqp3&raw=1" alt="Ảnh 1">
 </div>
         <div class="caption">Macintosh VietNam</div>
-      </div>
-
-     <div class="image-box">
-       <div onclick="openRightHalfPopup('https://techmaster.vn/posts?keyword=&category=88&author=' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/ymzmy3swhszvg79w1g9u6/Techmaster-VN-Mac-OSX.png?rlkey=blekdcukudwprtqfqyi8ieo4g&st=rxqwe42j&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Techmaster VN (Mac OSX)</div>
-      </div>
-
-      <div class="image-box">
-       <div onclick="openRightHalfPopup('https://brettterpstra.com/topic/macos/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/c93zzc2sziyxjrj08o9sv/Brettterpstra.png?rlkey=ws0aq1pz2fxb762tx60qmjr59&st=shb8fxxa&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Brettterpstra</div>
-      </div>
-
-      <div class="image-box">
-       <div onclick="openRightHalfPopup('https://www.macbed.com/' + chatParams, 'SmartApp Popup', window.innerWidth, 745)">
-       <img src="https://www.dropbox.com/scl/fi/d8gnak6jnmx65estx2t6i/Macbed.png?rlkey=yoqciaut41axy3z0ejdskpa8p&st=u4rmdo7w&raw=1" alt="Ảnh 1">
-</div>
-        <div class="caption">Macbed</div>
       </div>
 
 </div>
