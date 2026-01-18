cfb5b98046ae9a125dbcb8e55eda196e0a18c5e8
faceupduytruong <khobauonepiece@gmail.com>
2026-01-18 17:39:09 +0700

    Update CowimcThumbs.js

--- docs/CowimcThumbs.js
+++ docs/CowimcThumbs.js
@@ -45,6 +45,10 @@ const folderLinks = {
     url: 'https://www.facebook.com/profile.php?id=100057411881264&sk=reels_tab',
     thumbnail: 'https://www.dropbox.com/scl/fi/11en93i0nu77334trf7m7/D-y-N-u-n.jpg?rlkey=y32npnjziosmk68xcs9yv1ist&st=b9c9igwp&raw=1',
   },
+  'YÊU BẾP✅ (Esheep Kitchen family)': {
+    url: 'https://www.facebook.com/groups/557295677987925/',
+    thumbnail: 'https://www.dropbox.com/scl/fi/l4bafpvu1jeh3e2yhi5xk/Y-U-B-P-Esheep-Kitchen-family.jpg?rlkey=2uei1mj67gggqvai08zija3lz&st=21qyiwo2&raw=1',
+  },
   'Primitive Art': {
     url: 'https://www.facebook.com/primitiveart01/reels/',
     thumbnail: 'https://www.dropbox.com/scl/fi/4z5s73mcp90cv1fhe9w2q/Primitive-Art.jpg?rlkey=htd2io3xuwae0njmx71pxvv6p&st=noxw88af&raw=1',
