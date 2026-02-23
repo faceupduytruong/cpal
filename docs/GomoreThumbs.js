2b1af39085e53bf5f9103c82fbfad2ba817601d0
faceupduytruong <khobauonepiece@gmail.com>
2026-02-23 09:39:30 +0700

    Update GomoreThumbs.js

--- docs/GomoreThumbs.js
+++ docs/GomoreThumbs.js
@@ -12,7 +12,7 @@ const folders = {
 const folderLinks = {
   'Sách Về Tình Yêu': {
     url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Love/index.htm',
-    thumbnail: 'https://www.dropbox.com/scl/fi/0ts2ield0jhgkbw1pu3o2/S-ch-V-T-nh-Y-u.jpg?rlkey=uygmrrpefk0fcymbvn8qlxkpy&st=fh5v5pq2&raw=1'
+    thumbnail: 'https://www.dropbox.com/scl/fi/0ts2ield0jhgkbw1pu3o2/S-ch-V-T-nh-Y-u.jpg?rlkey=uygmrrpefk0fcymbvn8qlxkpy&st=fh5v5pq2&raw=1',
   },
   'Google Translate': {
     url: 'https://translate.google.com.vn/?lfhs=2&sl=en&tl=vi&op=translate',
