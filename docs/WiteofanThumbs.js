cef7a0568c2a9485f9e8b7529caf1c06aeb10ea0
faceupduytruong <khobauonepiece@gmail.com>
2026-02-25 23:43:47 +0700

    Update WiteofanThumbs.js

--- docs/WiteofanThumbs.js
+++ docs/WiteofanThumbs.js
@@ -10,9 +10,9 @@ const folders = {
 };
 
 const folderLinks = {
-  'Sách Nâng Cao Tự Tin': {
-    url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Confide/index.htm',
-    thumbnail: 'https://www.dropbox.com/scl/fi/wdqp4dut1x953ar8tnaex/Confide.webp?rlkey=h7iq454zqjyy1tdjmi4uw5lao&st=8dseqlnv&raw=1'
+  'Tuyển Tập Thơ': {
+    url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Poetry/index.htm',
+    thumbnail: 'https://www.dropbox.com/scl/fi/imj2rexi2hzcfzi1a5j8b/Tuy-n-T-p-Th.jpg?rlkey=80d6ggvusvpfptlmx84o073r6&st=ogxgaazn&raw=1'
   },
   'Google Translate': {
     url: 'https://translate.google.com.vn/?lfhs=2&sl=en&tl=vi&op=translate',
