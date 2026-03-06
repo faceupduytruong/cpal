0e724321c1463451ba844b4c1a179a1be5378b42
faceupduytruong <khobauonepiece@gmail.com>
2026-03-06 15:42:59 +0700

    Update enlingaimagBox.js

--- docs/enlingaimagBox.js
+++ docs/enlingaimagBox.js
@@ -4,12 +4,19 @@ container.innerHTML = `
 
      <!-- Hàng thứ 1 -->
     <div class="image-row glass-bg">
-    <h2 class="row-title">Easy-to-play Game</h2>
+    <h2 class="row-title">Easy-to-play Game</h2>    
       <div class="image-box">
        <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/47/2586/index.html?gameName=%E6%A4%8D%E7%89%A9%E6%89%93%E8%B4%A5%E5%83%B5%E5%B0%B8&gameId=2586&category=%E7%AD%96%E7%95%A5&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1549437&ad_source=iaa&djsource=l3vVSF&srcid=youyusiyouxi&lastTime=1772762719670&novipbtn=0&expe=10262_10272_10241_10217_10265_10273_10269_10200&iaaStyle=1&iaaAheadClick=1&hb=1', 'SmartApp Popup', window.innerWidth*2+45, 745)">
        <img src="https://s3m9.mdvdns.com/yiti/84cf099b2e831ee1f0fecfc4c1f081c3.png" alt="Ảnh 1">
 </div>
         <div class="caption">Cây trồng đánh bại thây ma</div>
+      </div>
+
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/87/1705/index.html?gameName=%E9%BB%84%E9%87%91%E7%9F%BF%E5%B7%A5&gameId=1705&category=%E4%BC%91%E9%97%B2&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1567222&ad_source=iaa&djsource=F8ljcx&srcid=youyusi&lastTime=1772762719670&novipbtn=0&expe=10262_10272_10241_10217_10265_10273_10269_10200&iaaStyle=1&iaaAheadClick=1&hb=1', 'SmartApp Popup', window.innerWidth*2+45, 745)">
+       <img src="https://s3m9.mdvdns.com/yiti/5b972755d1d2dc0500f94a3b646380aa.png_.webp" alt="Ảnh 1">
+</div>
+        <div class="caption">Những người khai thác vàng</div>
       </div>
 
       <div class="image-box">
@@ -21,7 +28,20 @@ container.innerHTML = `
 
  </div>
 
+ 
       <!-- Hàng thứ 2 -->
+    <div class="image-row glass-bg">
+    <h2 class="row-title">Intellectual Games</h2>
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/55/1090/index.html?gameName=%E6%A8%B1%E8%8A%B1%E6%A0%A1%E5%9B%AD%E6%A8%A1%E6%8B%9F%E5%99%A8&gameId=1090&category=%E6%A8%A1%E6%8B%9F&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1550256&ad_source=iaa&djsource=l3vVSF&srcid=youyusiyouxi&lastTime=1772762719670&novipbtn=0&expe=10262_10272_10241_10217_10265_10273_10269_10200&iaaStyle=1&iaaAheadClick=1&hb=1', 'SmartApp Popup', window.innerWidth*2+45, 745)">
+       <img src="https://s3m9.mdvdns.com/yiti/acc3fa6cd27a61e8aace685b64727556.png_.webp" alt="Ảnh 1">
+</div>
+        <div class="caption">Trình mô phỏng trường Sakura</div>
+      </div>
+
+ </div>
+
+      <!-- Hàng thứ 3 -->
     <div class="image-row glass-bg">
     <h2 class="row-title">Intellectual Games</h2>
       <div class="image-box">
@@ -31,23 +51,37 @@ container.innerHTML = `
         <div class="caption">Chú rắn nhỏ thích táo</div>
       </div>
 
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/58/1721/index.html?gameName=%E6%8B%AF%E6%95%91%E5%B0%8F%E7%BB%B5%E7%BE%8A&gameId=1721&category=%E4%BC%91%E9%97%B2&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1551243&ad_source=iaa&djsource=l3vVSF&srcid=youyusiyouxi&lastTime=1772762719670&novipbtn=0&expe=10262_10272_10241_10217_10265_10273_10269_10200&iaaStyle=1&iaaAheadClick=1&hb=1', 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://s3m9.mdvdns.com/yiti/9d484cbd9974f31f2f32fb377e6b2fa0.png_.webp" alt="Ảnh 1">
+</div>
+        <div class="caption">Giải cứu những chú cừu non</div>
+      </div>
+
  </div>
 
-    <!-- Hàng thứ 3 -->
+    <!-- Hàng thứ 4 -->
     <div class="image-row glass-bg">
     <h2 class="row-title">Architecture Design</h2>
       <div class="image-box">
   <div onclick="openRightHalfPopup('https://g.360.cn/index.html', 'SmartApp Popup', window.innerWidth*2+45, 745)">
-          <img src="https://www.dropbox.com/scl/fi/ggoqi0ltuol6dr3wcv28l/Tr-m-Game-1.png?rlkey=pkw8j9r8xf5phrbwxu8hz5c6o&st=ksq0u4cp&raw=1">
+          <img src="https://www.dropbox.com/scl/fi/ggoqi0ltuol6dr3wcv28l/Tr-m-Game-1.png?rlkey=pkw8j9r8xf5phrbwxu8hz5c6o&st=c5roloqt&raw=1">
 </div>
         <div class="caption">Trạm game 1</div>
+      </div>
+    
+      <div class="image-box">
+  <div onclick="openRightHalfPopup('https://g.360.cn/index.html', 'SmartApp Popup', window.innerWidth*2+45, 745)">
+          <img src="https://www.dropbox.com/scl/fi/enl69sscfuizkg8xpynyf/Tr-m-Game-2.png?rlkey=seqnl4ajx2noji0c8qkccsd0s&st=5izrwiqw&raw=1">
+</div>
+        <div class="caption">Trạm game 2</div>
       </div>
 
       <div class="image-box">
   <div onclick="openRightHalfPopup('https://g.360.cn/zt/summer2017.html', 'SmartApp Popup', window.innerWidth*2+45, 745)">
-          <img src="https://www.dropbox.com/scl/fi/enl69sscfuizkg8xpynyf/Tr-m-Game-2.png?rlkey=seqnl4ajx2noji0c8qkccsd0s&st=3febvw59&raw=1">
+          <img src="https://www.dropbox.com/scl/fi/b68j4quo75gc73nvjj0xt/Tr-m-Game-3.webp?rlkey=m4btmrh44uc7hygaq4ce9zzpu&st=vpj7ogua&raw=1">
 </div>
-        <div class="caption">Trạm game 2</div>
+        <div class="caption">Trạm game 3</div>
       </div>
       
 </div>
