7f735d4f36bdb2899519b20499f36f094713a6d0
faceupduytruong <khobauonepiece@gmail.com>
2026-03-06 10:35:35 +0700

    Update enlingaimagBox.js

--- docs/enlingaimagBox.js
+++ docs/enlingaimagBox.js
@@ -12,6 +12,13 @@ container.innerHTML = `
         <div class="caption">Cây trồng đánh bại thây ma</div>
       </div>
 
+      <div class="image-box">
+       <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/56/411/index.html?gameName=%E5%90%88%E6%88%90%E6%A4%8D%E7%89%A9%E6%89%93%E5%83%B5%E5%B0%B8&gameId=411&category=%E7%AD%96%E7%95%A5&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1551191&ad_source=iaa&djsource=l3vVSF&srcid=youyusiyouxi&lastTime=1772762719670&novipbtn=0&expe=10262_10272_10241_10217_10265_10273_10269_10200&iaaStyle=1&iaaAheadClick=1&hb=1&windowPmp=1', 'SmartApp Popup', window.innerWidth*2+45, 745)">
+       <img src="https://s3m9.mdvdns.com/yiti/3091709c79a2a5fc644af3a3f2f2671e.png" alt="Ảnh 1">
+</div>
+        <div class="caption">Tổng hợp thực vật để chống lại thây ma</div>
+      </div>
+
  </div>
 
     <!-- Hàng thứ 2 -->
