ef64fc776af1e7e386be19e13c9bcade46c1d5e0
faceupduytruong <khobauonepiece@gmail.com>
2026-03-08 19:18:53 +0700

    Update enlingaimagBox.js

--- docs/enlingaimagBox.js
+++ docs/enlingaimagBox.js
@@ -12,6 +12,13 @@ container.innerHTML = `
         <div class="caption">Những người khai thác vàng</div>
       </div>
 
+     <div class="image-box">
+       <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/63/1579/index.html?gameName=%E6%AC%A2%E4%B9%90%E7%89%88%E5%8D%95%E6%9C%BA%E6%96%97%E5%9C%B0%E4%B8%BB&gameId=1579&category=%E5%A8%B1%E4%B9%90&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1552655&ad_source=iaa&djsource=l3vVSF&srcid=youyusiyouxi&lastTime=1772762719670&novipbtn=0&expe=10261_10272_10241_10216_10266_10273_10270_10202&iaaStyle=1&iaaAheadClick=1&hb=1', 'SmartApp Popup', window.innerWidth*2+45, 745)">
+       <img src="https://s3m9.mdvdns.com/yiti/7fdacbced5d5e7f943d93f2ca4d7e50c.png_.webp" alt="Ảnh 1">
+</div>
+        <div class="caption">Card Duel</div>
+      </div>
+
       <div class="image-box">
        <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/56/507/index.html?gameName=%E7%A5%96%E7%8E%9B%E5%AE%8C%E6%95%B4%E7%89%881.0&gameId=507&category=%E4%BC%91%E9%97%B2&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1551191&ad_source=iaa&djsource=l3vVSF&srcid=youyusiyouxi&lastTime=1772762719670&novipbtn=0&expe=10261_10272_10241_10216_10266_10273_10270_10202&iaaStyle=1&iaaAheadClick=1&hb=1', 'SmartApp Popup', window.innerWidth, 745)">
        <img src="https://s3m9.mdvdns.com/yiti/be80cad9f445f7c9fda2cf01cd3e1b32.png_.webp" alt="Ảnh 1">
@@ -24,6 +31,13 @@ container.innerHTML = `
        <img src="https://s3m9.mdvdns.com/yiti/d62106b9bba2a3bb7075172f5743e655.png_.webp" alt="Ảnh 1">
 </div>
         <div class="caption">Chọn 3 con vật</div>
+      </div>
+
+     <div class="image-box">
+       <div onclick="openRightHalfPopup('https://fun.em0bu.cn/ssp/47/1024/index.html?gameName=%E6%89%93%E5%B7%A5%E4%BA%BA%E7%94%9F%E6%B4%BB%E6%A8%A1%E6%8B%9F&gameId=1024&category=%E4%BC%91%E9%97%B2&hasReal=1&qid=57aba2466da6c02ab0fa9f22f96fbe4e&cp=1549437&ad_source=iaa&djsource=l3vVSF&srcid=youyusiyouxi&lastTime=1772762719670&novipbtn=0&expe=10261_10272_10241_10216_10266_10273_10270_10202&iaaStyle=1&iaaAheadClick=1&hb=1', 'SmartApp Popup', window.innerWidth, 745)">
+       <img src="https://s3m9.mdvdns.com/yiti/daed1b78c5010f50f89ec115bbbc2776.png_.webp" alt="Ảnh 1">
+</div>
+        <div class="caption">Mô phỏng cuộc sống làm việc</div>
       </div>
 
  </div>
