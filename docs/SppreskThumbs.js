9e6c2f47c393cab7d2d0c1102c32b5eb81580ed6
faceupduytruong <khobauonepiece@gmail.com>
2026-03-25 16:28:56 +0700

    Update SppreskThumbs.js

--- docs/SppreskThumbs.js
+++ docs/SppreskThumbs.js
@@ -1,5 +1,5 @@
 const customThumbnails = {
-  'School topics': 'https://www.dropbox.com/scl/fi/m73gxcbw9fv0xn6oqwye7/School-topics.avif?rlkey=1vvvc8ljpindiwk3abptovbfq&st=cpd9sjwb&raw=1',
+  'School topics': 'https://www.dropbox.com/scl/fi/u5j4sef8ujs11jmt61cz6/School-topics.gif?rlkey=vm4h146yr0hdzy6xuerf85vng&st=o42fpmnb&raw=1',
   'Social topics': 'https://www.dropbox.com/scl/fi/hc8ecf4un7yfz2gjvx4al/Social-topics.webp?rlkey=46wezaze7ie67mzklblaxklma&st=46gvmb9r&raw=1',
   // thêm các mục khác nếu cần
 };
