698e10215c3a664b5188b35350debf29e84c0e97
faceupduytruong <khobauonepiece@gmail.com>
2026-04-01 14:45:56 +0700

    Update enlingaimagBox.js

--- docs/enlingaimagBox.js
+++ docs/enlingaimagBox.js
@@ -44,7 +44,7 @@ container.innerHTML = `
 
         <!-- Hàng thứ 2 -->
     <div class="image-row glass-bg">
-    <h2 class="row-title">Deep Sea Survival</h2>tránllatte
+    <h2 class="row-title">Deep Sea Survival</h2>
      <div class="image-box">
        <div onclick="openRightHalfPopup('https://html5.gamedistribution.com/rvvASMiM/9ff4c1c62da64af98d32fe1a1b44ac0a/index.html?gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL3d3dy5nYW1lZmxhcmUuY29tL2VtYmVkL21lZ2Etc2hhcmsvIiwicGFyZW50RG9tYWluIjoiZ2FtZWZsYXJlLmNvbSIsInRvcERvbWFpbiI6ImdhbWVmbGFyZS5jb20iLCJoYXNJbXByZXNzaW9uIjpmYWxzZSwibG9hZGVyRW5hYmxlZCI6dHJ1ZSwiaG9zdCI6Imh0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tIiwidmVyc2lvbiI6IjEuNS4xOCJ9', 'SmartApp Popup', window.innerWidth*2+45, 745)">
        <img src="https://www.dropbox.com/scl/fi/ammar1wnx9t9jgcsncbvg/Mega-Shark.webp?rlkey=xfekjbtzv3yajv7vsnowurfmb&st=w5e4hsp1&raw=1" alt="Ảnh 1">
