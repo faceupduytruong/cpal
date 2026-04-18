35706312d78457c36f411a4ef81af08a1b45ba5e
faceupduytruong <khobauonepiece@gmail.com>
2026-04-18 08:51:10 +0700

    Update callappbtncolr.js

--- java/callappbtncolr.js
+++ java/callappbtncolr.js
@@ -2,6 +2,33 @@
 
 const style = document.createElement('style');
 style.textContent = `
+
+  /* Style chung cho tất cả button */
+  button {
+    background-color: rgba(255,255,255,0.6); /* nền trong suốt */
+    border: 2px solid #4CAF50;     /* viền màu xanh lá */
+    color: black;                /* chữ màu đen */
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 8px;            /* bo góc */
+    transition: 0.3s;
+    }
+
+  /* Hiệu ứng hover */
+  button:hover {
+    background-color: #4CAF50;     /* khi hover nền xanh */
+    color: white;                  /* chữ trắng */
+    }
+
+  .button-container {
+    display: flex;
+    flex-wrap: wrap;          /* tự xuống hàng khi hết chỗ */
+    gap: 12px;                /* khoảng cách giữa các nút */
+    justify-content: center;  /* căn giữa theo chiều ngang */
+    margin-top: 0px;
+    }
+
   /* Notepad button */
   .btn-notepad {
     background-color: rgba(0, 0, 139, 0.6);
