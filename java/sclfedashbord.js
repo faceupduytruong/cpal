a9e17485c5679413b49b43f39e3168830c64b3b9
faceupduytruong <khobauonepiece@gmail.com>
2026-04-25 10:24:37 +0700

    Update sclfedashbord.js

--- java/sclfedashbord.js
+++ java/sclfedashbord.js
@@ -4,18 +4,22 @@ function renderFeed(playlist) {
 
   const card = document.createElement("div");
   card.className = "card";
+
+  // Chèn iframe từ playlist.html
   card.innerHTML = `
     <h3>${playlist.title}</h3>
     <p>Tác giả: ${playlist.author_name}</p>
-    <div>${playlist.html}</div>
-    <a href="${playlist.url}" target="_blank">Xem trên SoundCloud</a>
+    <img src="${playlist.thumbnail_url}" alt="${playlist.title}" style="max-width:200px;"/>
+    ${playlist.html}
+    <a href="${playlist.author_url}" target="_blank">Xem trên SoundCloud</a>
   `;
+
   feedContainer.appendChild(card);
 }
 
 async function fetchFeed() {
   try {
-    const username = document.getElementById("query").value; // chỉ nhập username
+    const username = document.getElementById("query").value;
     const response = await fetch(`http://127.0.0.1:8000/feed_soundcloud?username=${encodeURIComponent(username)}`);
     const data = await response.json();
     renderFeed(data);
