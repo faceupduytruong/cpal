edddeec3b2aa9b94eaed12f7539ac4f49261502d
faceupduytruong <khobauonepiece@gmail.com>
2026-04-25 10:56:00 +0700

    Update sclfedashbord.js

--- java/sclfedashbord.js
+++ java/sclfedashbord.js
@@ -5,12 +5,12 @@ function renderFeed(playlist) {
   const card = document.createElement("div");
   card.className = "card";
 
-  // Chèn iframe từ playlist.html
+  // Chèn đầy đủ thông tin từ oEmbed
   card.innerHTML = `
     <h3>${playlist.title}</h3>
     <p>Tác giả: ${playlist.author_name}</p>
     <img src="${playlist.thumbnail_url}" alt="${playlist.title}" style="max-width:200px;"/>
-    ${playlist.html}
+    ${playlist.html} <!-- iframe player -->
     <a href="${playlist.author_url}" target="_blank">Xem trên SoundCloud</a>
   `;
 
