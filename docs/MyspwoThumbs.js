2202757ff3a945ac2761662eb916f06017668d65
faceupduytruong <khobauonepiece@gmail.com>
2026-05-02 14:53:46 +0700

    Create MyspwoThumbs.js

---
+++ docs/MyspwoThumbs.js
@@ -0,0 +1,38 @@
+const customThumbnails = {
+  'Time': 'https://www.dropbox.com/scl/fi/o2t5xix5p7jefgkd6aybg/Time.jpg?rlkey=4seomewe2u7q22fk09wjmvx17&st=7hn1v56t&raw=1',
+  // thêm các mục khác nếu cần
+};
+
+// Danh sách thư mục và số lượng ảnh trong mỗi thư mục
+const folders = {
+  'Time': ['54088110141_a6dcd48bea_o.jpg', 'talkin_image_1729481035440.jpg', 'talkin_image_1730449853013.jpg', 'talkin_image_1730449854745.jpg', 'talkin_image_1730452491714.jpg', 'talkin_image_1730454953058.jpg', 'talkin_image_1730454954489.jpg', 'talkin_image_1730454957424.jpg', 'talkin_image_1730454966714.jpg', 'talkin_image_1730454969251.jpg', 'talkin_image_1733140473200.jpg', 'talkin_image_1735391001973.jpg', 'talkin_image_1735867431839.jpg', 'talkin_image_1737015117446.jpg', 'talkin_image_1737015118688.jpg', 'talkin_image_1737015121202.jpg', 'talkin_image_1737015123815.jpg', 'talkin_image_1737015147033.jpg', 'talkin_image_1737015148471.jpg', 'talkin_image_1737015150882.jpg', 'talkin_image_1737015153339.jpg', 'talkin_image_1737015184754.jpg', 'talkin_image_1737015186101.jpg', 'talkin_image_1737015188356.jpg', 'talkin_image_1737015190946.jpg', 'talkin_image_1737029907567.jpg', 'talkin_image_1737702106401.jpg', 'talkin_image_1737702107732.jpg', 'talkin_image_1737702110058.jpg', 'talkin_image_1737702112761.jpg', 'talkin_image_1739130158989.jpg', 'talkin_image_1753173710650.jpg', 'talkin_image_1753173712186.jpg', 'talkin_image_1753173714385.jpg', 'talkin_image_1753173716827.jpg', 'talkin_image_1753173902596.jpg', 'talkin_image_1753173904227.jpg', 'talkin_image_1753173906436.jpg', 'talkin_image_1753173908638.jpg', 'talkin_image_1753174087097.jpg', 'talkin_image_1753174088609.jpg', 'talkin_image_1753174090668.jpg', 'talkin_image_1753174093160.jpg', 'talkin_image_1756737549239.jpg', 'talkin_image_1756737660061.jpg', 'talkin_image_1756864921422.jpg', 'talkin_image_1760070344420.jpg'],
+  // thêm các mục khác nếu cần
+};
+
+const folderLinks = {
+  'Sách Khám Phá Bí Ẩn': {
+    url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Traveling/index.htm',
+    thumbnail: 'https://www.dropbox.com/scl/fi/ri7ard0iy4hyu9n987n7o/S-ch-Kh-m-Ph-B-n.jpg?rlkey=ln731vw27wb0m97w71wy9nf3z&st=q59bg052&raw=1'
+  },
+  'Google': {
+    url: 'https://translate.google.com.vn/?lfhs=2&sl=en&tl=vi&op=translate',
+    thumbnail: 'https://www.dropbox.com/scl/fi/go7hgrxhqjdlc8twaob54/Google.webp?rlkey=re0z5hmanpo8rp4xts7m24xck&st=riwgh2gl&raw=1'
+  },
+  'Nguyên Đạt Story': {
+    url: 'https://www.facebook.com/nguyendat2512s/reels/',
+    thumbnail: 'https://www.dropbox.com/scl/fi/aamk8bzq4xfgkxp854txq/Nguy-n-t-Story.jpg?rlkey=9lhk879uu45yzq9zav2epyton&st=ulk2o63h&raw=1',
+  },
+  'Bí Ẩn Tâm Linh': {
+    url: 'https://www.facebook.com/profile.php?id=61555482115414&sk=reels_tab',
+    thumbnail: 'https://www.dropbox.com/scl/fi/1rmkxo598f9rldagg7jn6/B-n-T-m-Linh.webp?rlkey=q0lkb22gxgp9six4j4v173pts&st=lequknzx&raw=1',
+  },
+  'Tâm Linh Huyền Bí': {
+    url: 'https://www.facebook.com/tamlinhplusvnc/reels/',
+    thumbnail: 'https://www.dropbox.com/scl/fi/qpxw6dp30bkn33tdu727r/T-m-Linh-Huy-n-B.jpg?rlkey=u16f7n6p6m9t2xajvr7ckhykh&st=fk6fjvd6&raw=1',
+  },
+  'NEW88 Truyện Ma Canh Tý': {
+    url: 'https://www.facebook.com/new88truyenmacanhty/reels/',
+    thumbnail: 'https://www.dropbox.com/scl/fi/a4hturk0c04wi5kb4o6zk/NEW88-Truy-n-Ma-Canh-T.jpg?rlkey=8aashzsm3w2zads7ew9lqq0py&st=duf40vn4&raw=1',
+  },
+  // thêm các mục khác nếu cần
+};
