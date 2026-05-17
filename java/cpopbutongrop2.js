479240624393e8e09828577acb7982d6e92b13f5
faceupduytruong <khobauonepiece@gmail.com>
2026-05-17 07:38:12 +0700

    Update cpopbutongrop2.js

--- java/cpopbutongrop2.js
+++ java/cpopbutongrop2.js
@@ -57,13 +57,13 @@ document.getElementById("engBtn").addEventListener("click", function () {
 
 /* Nhấn vào nút lyricsBtn 🐟 luân phiên mở YouTube và Bilibili */
 
-document.getElementById("lyricsBtn").addEventListener("click", function () {
-  document.getElementById("stationPopup").style.display = "flex";
-});
+//document.getElementById("lyricsBtn").addEventListener("click", function () {
+//  document.getElementById("stationPopup").style.display = "flex";
+//});
 
-document.getElementById("closeStationPopup").addEventListener("click", function () {
-  document.getElementById("stationPopup").style.display = "none";
-});
+//document.getElementById("closeStationPopup").addEventListener("click", function () {
+//  document.getElementById("stationPopup").style.display = "none";
+//});
 
 // Xử lý khi nhấn vào từng nút nền tảng
 document.querySelectorAll(".siteBtn").forEach(function (btn) {
