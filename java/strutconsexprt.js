27192c45185ddd84f28558160b1506d64ee57a04
faceupduytruong <khobauonepiece@gmail.com>
2026-05-18 10:00:54 +0700

    Update strutconsexprt.js

--- java/strutconsexprt.js
+++ java/strutconsexprt.js
@@ -142,7 +142,7 @@ async function openTool(toolName) {
 // Đảm bảo gọi được từ HTML
 window.openTool = openTool;
 
-import { createStationPopup } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal@b0f9601/docs/strutstationpop.js';
+import { createStationPopup } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal@c7257bf/docs/strutstationpop.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   // tạo popup
