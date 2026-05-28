8f027b5125956a6266b7e4956c7f4b124f32cd1d
faceupduytruong <khobauonepiece@gmail.com>
2026-05-29 00:36:34 +0700

    Add files via upload

---
+++ docs/OlychemiThumbs.js
@@ -0,0 +1,10 @@
+const customThumbnails = {
+  Oxidation: 'https://www.dropbox.com/scl/fi/g0o2nt9v66qckw3wamjfr/Oxidation.jpg?rlkey=oh3v3il427b9yo9asw0t410xm&st=zns13onm&raw=1',
+  // thêm các mục khác nếu cần
+};
+
+// Danh sách thư mục và số lượng ảnh trong mỗi thư mục
+const folders = {
+'Oxidation': ['2026-05-28_232044.png'],
+// thêm các mục khác nếu cần
+};
