2de223abf801394c53950caec9804ad7b633c5d5
faceupduytruong <khobauonepiece@gmail.com>
2026-05-29 11:58:09 +0700

    Update OlychemiThumbs.js

--- docs/OlychemiThumbs.js
+++ docs/OlychemiThumbs.js
@@ -1,10 +1,12 @@
 const customThumbnails = {
+  Composition: 'https://www.dropbox.com/scl/fi/ubkw2njw5uht0z8uazoe8/Composition.avif?rlkey=fviqt5b9m7pu2gxr5cer2l9d9&st=9h9qmams&raw=1',
   Oxidation: 'https://www.dropbox.com/scl/fi/g0o2nt9v66qckw3wamjfr/Oxidation.jpg?rlkey=oh3v3il427b9yo9asw0t410xm&st=zns13onm&raw=1',
   // thêm các mục khác nếu cần
 };
 
 // Danh sách thư mục và số lượng ảnh trong mỗi thư mục
 const folders = {
-'Oxidation': ['2026-05-28_232044.png'],
+'Composition': ['2026-05-29_090019.png'],
+'Oxidation': ['2026-05-28_232044.png', '2026-05-29_065401.png'],
 // thêm các mục khác nếu cần
 };
