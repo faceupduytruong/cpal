5786cdf7de9861cb9c398a9258294d1fb890e15c
faceupduytruong <khobauonepiece@gmail.com>
2026-05-29 17:01:34 +0700

    Update OlychemiThumbs.js

--- docs/OlychemiThumbs.js
+++ docs/OlychemiThumbs.js
@@ -1,5 +1,6 @@
 const customThumbnails = {
   Composition: 'https://www.dropbox.com/scl/fi/ubkw2njw5uht0z8uazoe8/Composition.avif?rlkey=fviqt5b9m7pu2gxr5cer2l9d9&st=9h9qmams&raw=1',
+  Moles: 'https://www.dropbox.com/scl/fi/3eg4ym9xjitiuxqwrgf5z/Moles.jpg?rlkey=35jmqmrmfs09rgokzzea3ykvx&st=8g9xky6u&raw=1',
   Oxidation: 'https://www.dropbox.com/scl/fi/g0o2nt9v66qckw3wamjfr/Oxidation.jpg?rlkey=oh3v3il427b9yo9asw0t410xm&st=zns13onm&raw=1',
   // thêm các mục khác nếu cần
 };
@@ -7,6 +8,7 @@ const customThumbnails = {
 // Danh sách thư mục và số lượng ảnh trong mỗi thư mục
 const folders = {
 'Composition': ['2026-05-29_090019.png'],
+'Moles': ['2026-05-29_125746.png'],
 'Oxidation': ['2026-05-28_232044.png', '2026-05-29_065401.png'],
 // thêm các mục khác nếu cần
 };
