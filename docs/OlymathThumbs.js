4c957d544bc008cb5bb15c0b81a81dae33616880
faceupduytruong <khobauonepiece@gmail.com>
2026-05-31 19:01:06 +0700

    Update OlymathThumbs.js

--- docs/OlymathThumbs.js
+++ docs/OlymathThumbs.js
@@ -1,6 +1,7 @@
 const customThumbnails = {
   Function: 'https://www.dropbox.com/scl/fi/rxytqn1kfi49n0nz480kv/Function.webp?rlkey=8zmfz2tndgqjifhwgumtuagu8&st=njmd77uv&raw=1',
   Integer: 'https://www.dropbox.com/scl/fi/e205t3mi73kh4q015vcjb/Integer.png?rlkey=d2clzsyh3ikao314yn6kambc7&st=e6bnhuta&raw=1',
+  Probability: 'https://www.dropbox.com/scl/fi/bwrkn63ndnzgcvelht6ru/Probability.jpg?rlkey=i6dg8g0iyd8gn4nyulwvgom1n&st=l1q4cijt&raw=1',
   Sequence: 'https://www.dropbox.com/scl/fi/n5hlv9d21bc1tffrrw0kj/Sequence.jpg?rlkey=dkx4uej2ofhyvkvr01rip422r&st=ol2346sz&raw=1',
   Trigonometry: 'https://www.dropbox.com/scl/fi/wip9zsbx50rfmxn8gwthh/Trigonometry.jpeg?rlkey=jjeby05ggp6fkltngclttuodo&st=gzm9kt1t&raw=1',
   Vector: 'https://www.dropbox.com/scl/fi/55wwh7eqh4w2xtj0g7189/Vector.jpg?rlkey=fgfmd4m46vnfdfja264vjuqd2&st=3594jubd&raw=1',
@@ -9,10 +10,11 @@ const customThumbnails = {
 
 // Danh sách thư mục và số lượng ảnh trong mỗi thư mục
 const folders = {
-'Function': ['2026-05-26_073500.png'],
-'Integer': ['2026-05-26_093444.png'],
-'Sequence': ['2026-05-25_234425.png', '2026-05-26_140046.png', '2026-05-28_205517.png', '2026-05-29_175746.png'],
-'Trigonometry': ['2026-05-26_150534.png'],
-'Vector': ['2026-05-28_154819.png'],
+'Function': ['Kiểm tra nghiệm thực của phương trình.png', 'Đinh lý Vi-ét.png'],
+'Integer': ['Tìm số nguyên thỏa mãn hệ phương trình.png'],
+'Probability': ['Tổ hợp chập k-n.png'],
+'Sequence': ['Chỉnh hợp chập k-n.png', 'Số nguyên tố, số nguyên và số tự nhiên.png', 'Tách phân số.png', 'Tổng cấp số cộng và cấp số nhân.png', 'Đạo hàm hàm số.png'],
+'Trigonometry': ['Phương trình lượng giác sinX = sinY.png'],
+'Vector': ['Tâm tỉ cự.png'],
 // thêm các mục khác nếu cần
 };
