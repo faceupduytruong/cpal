4bd2aeb173bf3924712c9a2b11f8926acfc43f41
faceupduytruong <khobauonepiece@gmail.com>
2026-05-31 19:07:49 +0700

    Update OlymathThumbs.js

--- docs/OlymathThumbs.js
+++ docs/OlymathThumbs.js
@@ -1,9 +1,9 @@
 const customThumbnails = {
   Function: 'https://www.dropbox.com/scl/fi/rxytqn1kfi49n0nz480kv/Function.webp?rlkey=8zmfz2tndgqjifhwgumtuagu8&st=njmd77uv&raw=1',
   Integer: 'https://www.dropbox.com/scl/fi/e205t3mi73kh4q015vcjb/Integer.png?rlkey=d2clzsyh3ikao314yn6kambc7&st=e6bnhuta&raw=1',
-  Probability: 'https://www.dropbox.com/scl/fi/bwrkn63ndnzgcvelht6ru/Probability.jpg?rlkey=i6dg8g0iyd8gn4nyulwvgom1n&st=l1q4cijt&raw=1',
+  Probability: 'https://www.dropbox.com/scl/fi/bwrkn63ndnzgcvelht6ru/Probability.jpg?rlkey=i6dg8g0iyd8gn4nyulwvgom1n&st=4ue8j96f&raw=1',
   Sequence: 'https://www.dropbox.com/scl/fi/n5hlv9d21bc1tffrrw0kj/Sequence.jpg?rlkey=dkx4uej2ofhyvkvr01rip422r&st=ol2346sz&raw=1',
-  Trigonometry: 'https://www.dropbox.com/scl/fi/wip9zsbx50rfmxn8gwthh/Trigonometry.jpeg?rlkey=jjeby05ggp6fkltngclttuodo&st=gzm9kt1t&raw=1',
+  Trigonometry: 'https://www.dropbox.com/scl/fi/wip9zsbx50rfmxn8gwthh/Trigonometry.jpeg?rlkey=jjeby05ggp6fkltngclttuodo&st=2abyohx4&raw=1',
   Vector: 'https://www.dropbox.com/scl/fi/55wwh7eqh4w2xtj0g7189/Vector.jpg?rlkey=fgfmd4m46vnfdfja264vjuqd2&st=3594jubd&raw=1',
   // thêm các mục khác nếu cần
 };
