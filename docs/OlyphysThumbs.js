c4ec083360cf8e3d03bc83acacf1bedf257f480a
faceupduytruong <khobauonepiece@gmail.com>
2026-06-03 22:05:58 +0700

    Update OlyphysThumbs.js

--- docs/OlyphysThumbs.js
+++ docs/OlyphysThumbs.js
@@ -2,7 +2,7 @@ const customThumbnails = {
   'Lò xo': 'https://www.dropbox.com/scl/fi/kmqi47r3m9l48f8gxgydc/Spring.webp?rlkey=nxj9hinw83kbsb1is5f3regwj&st=99ulgxw1&raw=1',
   'Mạch điện': 'https://www.dropbox.com/scl/fi/qags4k3nib5t0r41jxs3k/Circuit.jpg?rlkey=mt77hpxvofpolg79fno2g8i0y&st=21c9r5xy&raw=1',
   'Năng lượng': 'https://www.dropbox.com/scl/fi/a4iulf3y7nhtxvpc6qnxh/Efficiency.jpg?rlkey=8vqik1f5o1t2l76chdblyhcia&st=1fep6r1x&raw=1',
-  'Ròng rọc': 'https://www.dropbox.com/scl/fi/a4iulf3y7nhtxvpc6qnxh/Efficiency.jpg?rlkey=8vqik1f5o1t2l76chdblyhcia&st=1fep6r1x&raw=1',
+  'Ròng rọc': 'https://www.dropbox.com/scl/fi/ysa6zsjtytytl9xkw8351/R-ng-r-c.jpg?rlkey=qw49u2qjzlvrrwshxliir1sug&st=cx0sl9kq&raw=1',
   'Vật chuyển động': 'https://www.dropbox.com/scl/fi/drcsq6pa2u2qenw7y74kk/Transportation.jpg?rlkey=lkb66j0nsfhrr31yju9qchskl&st=yoxbv8tp&raw=1',
   Grammar: 'https://www.dropbox.com/scl/fi/vdfzsyxyjmayk1jrpma7e/Grammar.jpg?rlkey=6nac3oauj9w2ivbz7cuponom8&st=9g2f1s1h&raw=1',
   Health: 'https://www.dropbox.com/scl/fi/44a3z93cu05tmhzyzr7i3/Health.jpg?rlkey=xr36m336mjqec0was4ogohfyy&st=4pwimca2&raw=1',
