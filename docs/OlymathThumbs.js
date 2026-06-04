cca6ff26cbab31a0f4404d5d6aa1cdc5d49df163
faceupduytruong <khobauonepiece@gmail.com>
2026-06-04 15:04:29 +0700

    Update OlymathThumbs.js

--- docs/OlymathThumbs.js
+++ docs/OlymathThumbs.js
@@ -18,23 +18,3 @@ const folders = {
    'Vector': ['Tâm tỉ cự.png'],
    // thêm các mục khác nếu cần
 };
-
-const folderLinks = {
-  'Sách Kể Chuyện': {
-    url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Story/index.htm',
-    thumbnail: 'https://www.dropbox.com/scl/fi/mzn88e97lpg5o4wgqw3cx/S-ch-K-Chuy-n.webp?rlkey=71udzkkrr0jw2plf77s72ebxg&st=zbnf1h9g&raw=1',
-  },
-  'Google Translate': {
-    url: 'https://translate.google.com.vn/?lfhs=2&sl=en&tl=vi&op=translate',
-    thumbnail: 'https://www.dropbox.com/scl/fi/6686b8zwwloagxpcjanql/Google-Translate.jpg?rlkey=62s6qak72385hqbiod8414f1b&st=suqz7c3v&raw=1'
-  },
- 'GlassBox AI': {
-    url: 'https://www.facebook.com/profile.php?id=61576693687932&sk=reels_tab',
-    thumbnail: 'https://www.dropbox.com/scl/fi/sifdxv1ht763x2nel7g4o/GlassBox-AI.gif?rlkey=rbilphq4w3r6y0f7am2lacs3b&st=mr9tl1dp&raw=1'
-  },
- 'Phạm Mạnh Tuyến': {
-    url: 'https://www.youtube.com/@tuyenpham29792/videos',
-    thumbnail: 'https://www.dropbox.com/scl/fi/h87z2yni22z168litndio/Ph-m-M-nh-Tuy-n.jpg?rlkey=ae75na1ro7t02z0kmk0bdzhp7&st=00vqs84e&raw=1'
-  },
-  // thêm các mục khác nếu cần
-};
