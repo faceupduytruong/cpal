f9d0ee3184685af426024577578aa2065c06aba4
faceupduytruong <khobauonepiece@gmail.com>
2026-07-04 08:16:57 +0700

    Add files via upload

---
+++ docs/SatoailibThumbs.js
@@ -0,0 +1,14 @@
+const customThumbnails = {
+  'AI Extension': 'https://www.dropbox.com/scl/fi/l086p2ipn9zm7iks0yxzk/AI-Extension.png?rlkey=vvmrt81ovv1shwsrg5vatdqh8&st=453ng8bj&raw=1',
+  'Chuyên gia AI': 'https://www.dropbox.com/scl/fi/cyu8nrpb3rvmgrosk34us/Chuy-n-gia-AI.webp?rlkey=4ckjh4nf1zwvsqnd27ijuyaz2&st=nroxkvng&raw=1',
+  'Mindmap': 'https://www.dropbox.com/scl/fi/ob4i4d00nm7xtm3ay495q/Mindmap.png?rlkey=c1rff4vc0t6d13v2ng363cj7s&st=b4cdv1xn&raw=1', 
+  // thêm các mục khác nếu cần
+};
+
+// Danh sách thư mục và số lượng ảnh trong mỗi thư mục
+const folders = {
+  'AI Extension': ['Everything to Notebook extension.png'],
+  'Chuyên gia AI': ['Chuyên gia chiến lược nghiên cứu của tôi.jpg'],
+  'Mindmap': ['Vẽ Mindmap màu sắc cho con nít.png'],
+// thêm các mục khác nếu cần
+};
