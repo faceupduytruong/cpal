360c94777a64537672cab6072339cb50c5682f19
faceupduytruong <khobauonepiece@gmail.com>
2026-07-06 22:40:35 +0700

    Create OlyliterThumbs.js

---
+++ docs/OlyliterThumbs.js
@@ -0,0 +1,22 @@
+const customThumbnails = {
+   'Tốt nghiệp trung học phổ thông': 'https://www.dropbox.com/scl/fi/frygnc44v3bpghggcd4p0/T-t-nghi-p-trung-h-c-ph-th-ng.png?rlkey=3xmk2u712xjfmred49atul4jo&st=3d1m54az&raw=1',
+   // thêm các mục khác nếu cần
+};
+
+// Danh sách thư mục và số lượng ảnh trong mỗi thư mục
+const folders = {
+   'Tốt nghiệp trung học phổ thông': ['Làm thế nào để có những Steve Jobs Việt Nam.jpg'],
+   // thêm các mục khác nếu cần
+};
+
+const folderLinks = {
+  'Sách Kể Chuyện': {
+    url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Story/index.htm',
+    thumbnail: 'https://www.dropbox.com/scl/fi/mzn88e97lpg5o4wgqw3cx/S-ch-K-Chuy-n.webp?rlkey=71udzkkrr0jw2plf77s72ebxg&st=zbnf1h9g&raw=1',
+  },
+  'Google Translate': {
+    url: 'https://translate.google.com.vn/?lfhs=2&sl=en&tl=vi&op=translate',
+    thumbnail: 'https://www.dropbox.com/scl/fi/6686b8zwwloagxpcjanql/Google-Translate.jpg?rlkey=62s6qak72385hqbiod8414f1b&st=suqz7c3v&raw=1',
+  },
+  // thêm các mục khác nếu cần
+};
