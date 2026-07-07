6fd07f52a7d67f83f92a1271bb266d29031822ca
faceupduytruong <khobauonepiece@gmail.com>
2026-07-07 14:26:21 +0700

    Add new thumbnails and update folder structure

--- docs/SatoailibThumbs.js
+++ docs/SatoailibThumbs.js
@@ -1,5 +1,6 @@
 const customThumbnails = {
   'AI Extension': 'https://www.dropbox.com/scl/fi/l086p2ipn9zm7iks0yxzk/AI-Extension.png?rlkey=vvmrt81ovv1shwsrg5vatdqh8&st=453ng8bj&raw=1',
+  'AI Website': 'https://www.dropbox.com/scl/fi/6ay2bqz4byaa30ktyxtkk/AI-Website.png?rlkey=c6yjq1oes8dewiwjue4dq8zrb&st=jaklocnb&raw=1',
   'Chuyên gia AI': 'https://www.dropbox.com/scl/fi/cyu8nrpb3rvmgrosk34us/Chuy-n-gia-AI.webp?rlkey=4ckjh4nf1zwvsqnd27ijuyaz2&st=nroxkvng&raw=1',
   'Mindmap': 'https://www.dropbox.com/scl/fi/ob4i4d00nm7xtm3ay495q/Mindmap.png?rlkey=c1rff4vc0t6d13v2ng363cj7s&st=b4cdv1xn&raw=1', 
   // thêm các mục khác nếu cần
@@ -8,7 +9,8 @@ const customThumbnails = {
 // Danh sách thư mục và số lượng ảnh trong mỗi thư mục
 const folders = {
   'AI Extension': ['Everything to Notebook extension.png'],
-  'Chuyên gia AI': ['Chuyên gia chiến lược nghiên cứu của tôi.jpg'],
+  'AI Website': ['Baidu AI Agent Builder.png'],
+  'Chuyên gia AI': ['Prompt chuyên gia chiến lược nghiên cứu của tôi.jpg', 'Prompt người sáng tác truyện tranh trẻ em.jpg', 'Prompt study guide toàn diện.png'],
   'Mindmap': ['Vẽ Mindmap màu sắc cho con nít.png'],
-// thêm các mục khác nếu cần
+  // thêm các mục khác nếu cần
 };
