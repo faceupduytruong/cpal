12a0d30167c336f3ea2429b24628cff86a9d4288
faceupduytruong <khobauonepiece@gmail.com>
2026-07-22 20:18:56 +0700

    Update OlybioloThumbs.js

--- docs/OlybioloThumbs.js
+++ docs/OlybioloThumbs.js
@@ -16,3 +16,17 @@ const folders = {
   'Tiến hóa loài': ['Thí nghiệm cóc độc và rắn không độc.png'],
   // thêm các mục khác nếu cần
 };
+
+const folderLinks = {
+  'Sách Kể Chuyện': {
+    url: 'https://lighthearted-mediafire-ebook-d0d92e.netlify.app/Story/index.htm',
+    thumbnail: 'https://www.dropbox.com/scl/fi/mzn88e97lpg5o4wgqw3cx/S-ch-K-Chuy-n.webp?rlkey=71udzkkrr0jw2plf77s72ebxg&st=zbnf1h9g&raw=1',
+  },
+  'Google Translate': {
+    url: 'https://translate.google.com.vn/?lfhs=2&sl=en&tl=vi&op=translate',
+    thumbnail: 'https://www.dropbox.com/scl/fi/6686b8zwwloagxpcjanql/Google-Translate.jpg?rlkey=62s6qak72385hqbiod8414f1b&st=suqz7c3v&raw=1',
+  },
+  'Sinh thầy Tuấn': {
+    url: 'https://www.youtube.com/@SinhthayTuan/videos',
+    thumbnail: 'https://www.dropbox.com/scl/fi/jtxzc49p9u51qucdgybv6/Sinh-th-y-Tu-n.jpg?rlkey=7bregpzm3u0l3u2fxkz9tpjke&st=vd8dcpo2&raw=1',
+  },
