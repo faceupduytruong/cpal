3c7ebb5b33786371d02c44ecd35d7eaf814d457c
faceupduytruong <khobauonepiece@gmail.com>
2026-07-26 18:54:29 +0700

    Update OlybioloThumbs.js

--- docs/OlybioloThumbs.js
+++ docs/OlybioloThumbs.js
@@ -1,5 +1,6 @@
 const customThumbnails = {
   'Chọn lọc tự nhiên': 'https://www.dropbox.com/scl/fi/0hovz0whww3m160w4ck9w/Ch-n-l-c-t-nhi-n.jpg?rlkey=zz8jbca8rrztjvrgsvifl52kd&st=fibwofef&raw=1',
+  'Các loài động vật': 'https://www.dropbox.com/scl/fi/1d5f5yfhahg66sy0uposi/C-c-lo-i-ng-v-t.webp?rlkey=xuv5qfd1nvahihfjmxxfmw6sf&st=t8l9rw0p&raw=1',
   'Phân chia tế bào': 'https://www.dropbox.com/scl/fi/2csmjohhjlva8r96svwke/Ph-n-chia-t-b-o.jpg?rlkey=3fpgzwk6aep4zj3synvvcxubp&st=02p3il0v&raw=1',
   'Phối giống di truyền': 'https://www.dropbox.com/scl/fi/z75aqyaizpk7ywf5bxd06/Ph-i-gi-ng-di-truy-n.jpg?rlkey=i5ym2g4p4oaaxu1lq8oxgvdcd&st=0vonw1x4&raw=1',
   'Thí nghiệm sinh học': 'https://www.dropbox.com/scl/fi/8mi0y3ow9muub7z5zdo49/Th-nghi-m-sinh-h-c.jpg?rlkey=q1p9f9b8zx10kft17n9irx52r&st=7xcq54x4&raw=1',
@@ -10,9 +11,11 @@ const customThumbnails = {
 // Danh sách thư mục và số lượng ảnh trong mỗi thư mục
 const folders = {
   'Chọn lọc tự nhiên': ['Kích thước mỏ giảm của chim sẻ Geospiza Fortis.png'],
+  'Các loài động vật': ['Cảm ứng ở động vật.png'],
   'Phân chia tế bào': ['Tái bản phân tử DNA của vi khuẩn.png'],
   'Phối giống di truyền': ['Số loại giao tử gene.png', 'Tương tác gene của chuột nhảy Meriones Unguiculatus.png', 'Tần số allele qua các thế hệ của loài thực vật lưỡng bội.png'],
   'Thí nghiệm sinh học': ['Tạo DNA tái tổ hợp có dấu phóng xạ.png'],
+  'Thực vật cây trồng': ['Quang hợp và năng suất cây trồng.png'],
   'Tiến hóa loài': ['Thí nghiệm cóc độc và rắn không độc.png'],
   // thêm các mục khác nếu cần
 };
