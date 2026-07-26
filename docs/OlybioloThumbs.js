791debb495d912a000395e9f9c4c0cc16f1692e7
faceupduytruong <khobauonepiece@gmail.com>
2026-07-26 19:23:10 +0700

    Update OlybioloThumbs.js

--- docs/OlybioloThumbs.js
+++ docs/OlybioloThumbs.js
@@ -4,6 +4,7 @@ const customThumbnails = {
   'Phân chia tế bào': 'https://www.dropbox.com/scl/fi/2csmjohhjlva8r96svwke/Ph-n-chia-t-b-o.jpg?rlkey=3fpgzwk6aep4zj3synvvcxubp&st=02p3il0v&raw=1',
   'Phối giống di truyền': 'https://www.dropbox.com/scl/fi/z75aqyaizpk7ywf5bxd06/Ph-i-gi-ng-di-truy-n.jpg?rlkey=i5ym2g4p4oaaxu1lq8oxgvdcd&st=0vonw1x4&raw=1',
   'Thí nghiệm sinh học': 'https://www.dropbox.com/scl/fi/8mi0y3ow9muub7z5zdo49/Th-nghi-m-sinh-h-c.jpg?rlkey=q1p9f9b8zx10kft17n9irx52r&st=7xcq54x4&raw=1',
+  'Thực vật cây trồng': 'https://www.dropbox.com/scl/fi/78vmyk0xyvsr6vsvf9uyy/Th-c-v-t-c-y-tr-ng.png?rlkey=aqa09wrcbycpo0z7kz8w03sdj&st=xxro8jqf&raw=1',
   'Tiến hóa loài': 'https://www.dropbox.com/scl/fi/4ab21jx12at037h8ugoqd/Ti-n-h-a-lo-i.webp?rlkey=t51h9tmdbwu5mzdno7scsvb6f&st=eki6n2yv&raw=1', 
   // thêm các mục khác nếu cần
 };
