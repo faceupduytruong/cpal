d4134f2acc5f4e8a7ec4f586723c2062119cafd6
faceupduytruong <khobauonepiece@gmail.com>
2026-09-11 14:58:55 +0700

    Update ChihodThumbs.js

--- docs/ChihodThumbs.js
+++ docs/ChihodThumbs.js
@@ -30,6 +30,10 @@ const folderLinks = {
     url: 'https://www.facebook.com/dicungcon.official',
     thumbnail: 'https://www.dropbox.com/scl/fi/kwsmchjvg4168vbx7mrx9/i-C-ng-Con.jpg?rlkey=9ofem2ljl3jlf7iyr1uiu141x&st=t2813j7h&raw=1',
   },
+  'Kidsup': {
+    url: 'https://www.kidsup.net/blog/ba-me-nuoi-day-con/',
+    thumbnail: 'https://www.dropbox.com/scl/fi/wb6dg5qo4ud5cmo6sm56u/Kidsup.jpeg?rlkey=xqor04ecikgcees1g2hp653yf&st=6wj00a35&raw=1',
+  },
   'HYPPO Kids': {
     url: 'https://www.facebook.com/HYPPOforkids',
     thumbnail: 'https://www.dropbox.com/scl/fi/fb7u0djjfehak1cj9e4gj/HYPPO-Kids.png?rlkey=jvoxgy2xkycjd28qh3wsue8ye&st=9rm1vyd3&raw=1',
