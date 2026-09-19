e54376de0e5ec6483482ab3e26e028d5d884483a
faceupduytruong <khobauonepiece@gmail.com>
2026-09-19 22:43:44 +0700

    Update clipbcelwehtl3.js

--- java/clipbcelwehtl3.js
+++ java/clipbcelwehtl3.js
@@ -67,10 +67,10 @@ body::-webkit-scrollbar-thumb {
 
 
 <!-- 👉 Nhúng js cho việc dropzone và hiển thị clipboard -->
-<script src="https://cdn.jsdelivr.net/gh/faceupduytruong/cpal@b33f827/java/imgdrpzonare1.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/java/imgdrpzonare1.js"></script>
 
 <!-- 👉 Nhúng js cho việc dropzone và hiển thị clipboard -->
-<script src="https://cdn.jsdelivr.net/gh/faceupduytruong/cpal@7e05c66/java/imgdrpzonare2.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/java/imgdrpzonare2.js"></script>
 
 <div class="content">${text}</div>
 
