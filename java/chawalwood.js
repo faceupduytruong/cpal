9de832d44d810668e31b54848cd578b067cc1dfd
faceupduytruong <khobauonepiece@gmail.com>
2026-09-19 09:11:16 +0700

    Update chawalwood.js

--- java/chawalwood.js
+++ java/chawalwood.js
@@ -1,4 +1,4 @@
-    import { woodSources } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/docs/woodSources.js';
+    import { woodSources } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/move/woodSources.js';
     window.woodSources = woodSources; // Gán vào window để dùng ở script thường
 
     const video = document.getElementById('bgVideo');
