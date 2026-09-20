30e4a820c41e984e819b8e3e877b0d67f35807f7
faceupduytruong <khobauonepiece@gmail.com>
2026-09-20 08:46:10 +0700

    Update btntglevidcolfont.js

--- java/btntglevidcolfont.js
+++ java/btntglevidcolfont.js
@@ -1,7 +1,7 @@
 // 👉 Nhúng dữ liệu từ các file JS trên GitHub
 import { bgVideos } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/move/mxktSources.js';
-import { chineseArtists } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/docs/chiengartlist.js';
-import { artisticFonts } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/docs/artisticfonlist2.js';
+import { chineseArtists } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/move/chiengartlist.js';
+import { artisticFonts } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/move/artisticfonlist2.js';
 import { colors } from 'https://cdn.jsdelivr.net/gh/faceupduytruong/cpal/move/coloarramodif.js';
 
 let currentIndex = 0;
